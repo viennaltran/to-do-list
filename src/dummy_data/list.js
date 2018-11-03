743830ae207dc5675c2c22dd46ecec1b406d0f0e
Scott Bowler <scott.bowler@learningfuze.com>
2018-11-02 17:56:27 -0700

    Initial project setup complete with dummy data

---
+++ src/dummy_data/list.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        title: 'Go to store',
+        details: 'Get eggs, bread, milk, and Diet Coke',
+        _id: '001'
+    },
+    {
+        title: 'Wash car',
+        details: 'Wash, wax, and clean the inside',
+        _id: '002'
+    },
+    {
+        title: 'Learn React',
+        details: 'Go to school to learn ReactJS',
+        _id: '003'
+    },
+    {
+        title: 'Feed the bear',
+        details: 'She is very hangry',
+        _id: '004'
+    }
+];
