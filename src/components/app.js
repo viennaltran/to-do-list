642ed335cd2a9288de3f62b0cbd7ca10452d0d52
viennaltran <lanvienhtran@gmail.com>
2018-11-06 17:48:27 -0800

    completed delete item functionality

--- src/components/app.js
+++ src/components/app.js
@@ -17,6 +17,18 @@ class App extends Component{
         }
     }
 
+    deleteItem = (index) => {
+        const listCopy =this.state.list.slice();
+
+        listCopy.splice(index,1);
+
+        this.setState({
+            list:listCopy
+        });
+
+
+    }
+
     addItem = (item) => {
 
         item._id=randomString(8);
@@ -45,7 +57,7 @@ class App extends Component{
                 <h1 className="center">To Do List</h1>
                 
                 <AddItem add={this.addItem}/>
-                <List data={this.state.list}/>
+                <List delete={this.deleteItem} data={this.state.list}/>
             </div>
         );
     }
--- src/components/list.js
+++ src/components/list.js
@@ -7,7 +7,16 @@ class List extends Component {
     render(){
 
         const listElements = this.props.data.map((item, index) => {
-            return <li className="collection-item" key={item._id}>{item.title}</li>;
+            return (
+                <li className="collection-item row" key={item._id}>
+                    <div className="col s8">
+                        {item.title}
+                    </div>
+                    <div className= "col s4 right-align">
+                        <button onClick = {() => {this.props.delete(index)}}className="btn red darken-2">Delete</button>
+                    </div>
+                </li>  
+                );
         });
 
         return (
